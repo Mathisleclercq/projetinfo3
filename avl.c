c092fa70306b9b84b0035e876e68615b5aa6985d
Mathisleclercq <matlec2206@gmail.com>
2025-12-19 12:14:36 +0100

    Update avl.c

--- avl.c
+++ avl.c
@@ -4,13 +4,14 @@
 typedef struct AVL {
     char* identifiant;
     float valeur;
+    float valeurreal;
     int equilibre;
     struct AVL* gauche;
     struct AVL* droite;
 } AVL;
 
 
-AVL* creerNoeud(char* id, float val) {
+AVL* creerNoeud(char* id, float val,float valreal) {
     AVL* nouveau = (AVL*) malloc(sizeof(AVL));
     if (nouveau == NULL) {
         printf("Erreur allocation mémoire\n");
@@ -18,6 +19,7 @@ AVL* creerNoeud(char* id, float val) {
     }
     nouveau->identifiant =  strdup(id);
     nouveau->valeur = val;
+    nouveau->valeurreal=valreal;
     nouveau->equilibre = 0;
     nouveau->gauche = NULL;
     nouveau->droite = NULL;
@@ -91,19 +93,20 @@ AVL* equilibrerAVL(AVL* a) {
 }
 
 
-AVL* insertionAVL(AVL* a, char* id, float val, int* h) {
+AVL* insertionAVL(AVL* a, char* id, float val,float valreal, int* h) {
     if (a == NULL) {
         *h = 1;
-        return creerNoeud(id, val);
+        return creerNoeud(id, val, valreal);
     }
     if (strcmp(id, a->identifiant) < 0) {
-        a->gauche = insertionAVL(a->gauche, id, val, h);
+        a->gauche = insertionAVL(a->gauche, id, val,valreal, h);
         *h = -*h;
     } else if (strcmp(id, a->identifiant) > 0) {
-        a->droite = insertionAVL(a->droite, id, val, h);
+        a->droite = insertionAVL(a->droite, id, val,valreal, h);
     } else {
         *h = 0;
        a->valeur = a->valeur + val;
+       a->valeurreal = a->valeurreal + valreal;
         return a; 
     }
     if (*h != 0) {
@@ -124,32 +127,58 @@ void Ecrire_fichier(AVL* a,FILE* f){
         Ecrire_fichier(a->gauche,f);
     }
 }
+void Ecrire_fichierR(AVL* a,FILE* f){
+    if(a!=NULL){    
+        Ecrire_fichierR(a->droite,f);
+        fprintf(f,"%s;%f\n",a->identifiant,a->valeurreal);
+        Ecrire_fichierR(a->gauche,f);
+    }
+}
 int main(){
-    FILE *capacite = fopen("val.txt", "r");
+    FILE *capacite = fopen("sources_volumes.dat", "r");
     if (capacite == NULL){
     	printf("erreur ouverture");
       exit(1);
     }
-    FILE *nom = fopen("id.txt", "r");
+    FILE *nom = fopen("plants_id.dat", "r");
     if (nom == NULL){
     	printf("erreur ouverture");
       exit(1);
     }
-AVL* a = NULL;
-int h =0;
-char id[50];
-float val;
-
-while ( fscanf(capacite, "%f", &val) == 1 && fgets(id, 49, nom)!= NULL) {
-	id[strcspn(id, "\r\n")] = '\0';
-	a = insertionAVL(a, id, val, &h);
-}
-
-FILE* f = fopen("vol_captation.txt", "w");
+  FILE *Ffuite = fopen("sources_leaks.dat", "r");
+    if (Ffuite == NULL){
+    	printf("erreur ouverture");
+      exit(1);
+      }
+  FILE* f = fopen("vol_captation.txt", "w");
 	if(f == NULL){
 	printf("erreur ouverture fichier");
 	exit(1);
 	}
+FILE* f2 = fopen("vol_traitement.tmp", "w");
+	if(f == NULL){
+	printf("erreur ouverture fichier");
+	exit(1);
+	}
+AVL* a = NULL;
+int h =0;
+char id[50];
+float val;
+float fuite;
+float valreal;
+fgets(id, 49, nom);
+fprintf(f,"%s",id);
+fgets(id, 49,capacite);
+fprintf(f2,"%s",id);
+fgets(id, 49, Ffuite);
+while ( fscanf(capacite, "%f", &val) == 1 && fgets(id, 49, nom)!= NULL && fscanf(Ffuite, "%f", &fuite)==1) {
+	id[strcspn(id, "\r\n")] = '\0';
+	valreal = val * (1 - fuite/100);
+	printf("%f\n",valreal);
+	a = insertionAVL(a, id, val,valreal, &h);
+}
+
+Ecrire_fichierR(a,f2);
 Ecrire_fichier(a,f);
 fclose(f);
  fclose(nom);
